9e3ece6eacc34076961ac1a6e288cfa3aac774ba
Raman41k <romangugel@gmail.com>
2022-09-14 18:36:40 +0300

    Clean App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import Characters from "./components/Characters/Characters";
-import Character from "./components/Character/Character";
-import Simpson from "./components/Simpsons/Simpson";
-import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
       <div>
-        <Characters/>
-          <Hero id={1} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
-          <Hero id={2} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
-          <Hero id={3} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
-          <Hero id={4} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
-          <Hero id={5} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
-          <Hero id={6} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
-          <Hero id={7} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"} image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
-          <Simpson/>
+
       </div>
   );
 }
